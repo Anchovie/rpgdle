2b8d85a2dd219817a0d3f73eb58fd3c6c0f665af
Anchovie <panajis.rantala@gmail.com>
2022-04-25 23:34:01 +0300

    Heroku postgresql fix

--- rpgdle/settings.py
+++ rpgdle/settings.py
@@ -1,5 +1,10 @@
 import os
+import re
 
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = uri
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
