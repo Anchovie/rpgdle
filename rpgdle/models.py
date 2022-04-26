18360f3917478ae4cd5816631560f33a1d4110ff
Anchovie <panajis.rantala@gmail.com>
2022-04-26 12:03:51 +0300

    db

--- rpgdle/models.py
+++ rpgdle/models.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(255))
     admin = db.Column(db.Boolean)
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
