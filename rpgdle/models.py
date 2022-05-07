099241ce150a4817fe8faa286d0161c130a8406a
Anchovie <panajis.rantala@gmail.com>
2022-05-07 12:57:51 +0300

    Added unique constraint to User name

--- rpgdle/models.py
+++ rpgdle/models.py
@@ -6,7 +6,7 @@ from .extensions import db
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(255))
     admin = db.Column(db.Boolean)
     created = db.Column(db.DateTime, default=datetime.utcnow)
